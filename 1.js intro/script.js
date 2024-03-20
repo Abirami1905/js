f1839886c5dab7b4dd911c0deeac089ef6eac059
Abirami1905 <146990167+Abirami1905@users.noreply.github.com>
2024-03-19 16:27:41 -0930

    Create script.js

---
+++ 1.js intro/script.js
@@ -0,0 +1,19 @@
+//this is an alert
+
+        //single line comment
+        /*multi line comment
+        JSintoduction*/
+        
+        //variables - 8 bytes [1 byte= 64 bits]
+
+        let variable = 0 //initialization
+        //assignment
+        //dynamically typed language
+       
+        Amount=10  //binary code - 1010
+        console.log("Your bill amount is Rs."+Amount);
+        alert("Welcome to the World of JavaScript");
+
+        const pi =3.14  //constant
+        var val = 10
+        
