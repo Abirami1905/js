725f168e122f82f7cac4ce0260f3f736c067581a
Abirami1905 <146990167+Abirami1905@users.noreply.github.com>
2024-03-22 07:20:55 -0930

    Create 2-script.js

---
+++ 1.js intro/2-script.js
@@ -0,0 +1,28 @@
+  // //this is an alert
+
+        // //single line comment
+        // /*multi line comment
+        // JSintoduction*/
+        
+        // //variables - 8 bytes [1 byte= 64 bits]
+
+        // let variable = 0 //initialization
+        // //assignment
+        // //dynamically typed language
+       
+        // Amount=10  //binary code - 1010
+        // console.log("Your bill amount is Rs."+Amount);
+        // alert("Welcome to the World of JavaScript");
+
+        // const pi =3.14  //constant
+        // var val = 10
+
+
+        let name = prompt("What's your name?")
+        console.log('hi', name)
+
+       let tickets=prompt('How many tickets do you have?')
+       console.log("you shold pay",Number(tickets)*20)
+
+      let a=prompt("enter a number:")
+      console.log(Number(a)+10)
